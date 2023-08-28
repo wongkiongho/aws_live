b4c446203632faf9addb969508546fe4563d5b8d
wongkiongho <125861978+wongkiongho@users.noreply.github.com>
2023-08-28 11:49:55 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.cgbw848jqsd7.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "wongkiongho-employee"
+customregion = "us-east-1"
+
